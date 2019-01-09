a874dab565fd3121a5c9dbcc1fb4da9aaf5d8568
Marian Ciortea <marian.ciortea@yahoo.com>
2019-01-09 21:29:11 +0200

    Simplified array rotation solution.

--- src/RotateArray/RotateArraySolution.cs
+++ src/RotateArray/RotateArraySolution.cs
@@ -6,37 +6,17 @@ namespace RotateArray
     {
         public int[] RotateArray(int[] arr, int k)
         {
-            // If k = 0, array is empty or contains one element or k is equal to length of the array
-            // we don't need to rotate the array and we simply return it.
-            if (k == 0 || arr.Length <= 1 || k == arr.Length)
-            {
-                return arr;
-            }
+            int[] rotatedArr = new int[arr.Length];
 
-            // If k is higher than length of the array then we calculate the actual number
-            // of positions to rotate the array.
-            // e.g: if array length is 4 and k = 5 then we basically need to rotate the array one time.
-            if (k > arr.Length)
-            {
-                k = k % arr.Length;
-            }
+            k = k % arr.Length;
+
+            int sourceIndex = arr.Length - k;
+            Array.Copy(arr, sourceIndex, rotatedArr, 0, k);
 
             int length = arr.Length - k;
+            Array.Copy(arr, 0, rotatedArr, k, length);
 
-            var arr1 = new int[length];
-            Array.Copy(arr, arr1, length);
-            Array.Reverse(arr1);
-
-            var arr2 = new int[k];
-            Array.Copy(arr, length, arr2, 0, k);
-            Array.Reverse(arr2);
-
-            var resultedAr = new int[arr.Length];
-            Array.Copy(arr1, resultedAr, arr1.Length);
-            Array.Copy(arr2, 0, resultedAr, arr1.Length, arr2.Length);
-            Array.Reverse(resultedAr);
-
-            return resultedAr;
+            return rotatedArr;
         }
     }
 }
