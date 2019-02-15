725422c69dc20314fc9dff6c21c9862fc40f158f
maciortea <marian.ciortea@yahoo.com>
2019-02-15 10:30:51 +0200

    Add bottom-up solution to Longest Common Substring problem.

--- src/LongestCommonSubstring/LongestCommonSubstringSolution.cs
+++ src/LongestCommonSubstring/LongestCommonSubstringSolution.cs
@@ -4,27 +4,48 @@ namespace LongestCommonSubstring
 {
     public class LongestCommonSubstringSolution
     {
-        public int GetLongestCommonSubstring(string first, string second)
+        public int GetLongestCommonSubstringBottomUp(string str1, string str2)
         {
-            return GetLongestCommonSubstring(first, second, 0, 0);
+            int length1 = str1.Length + 1;
+            int length2 = str2.Length + 1;
+            int[,] matrix = new int[length1, length2];
+            for (int i = 1; i < length1; i++)
+            {
+                for (int j = 1; j < length2; j++)
+                {
+                    if (str1[i - 1] == str2[j - 1])
+                    {
+                        matrix[i, j] = 1 + matrix[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        matrix[i, j] = Math.Max(matrix[i - 1, j], matrix[i, j - 1]);
+                    }
+                }
+            }
+
+            return matrix[length1 - 1, length2 - 1];
         }
 
-        private int GetLongestCommonSubstring(string first, string second, int firstIndex, int secondIndex)
+        public int GetLongestCommonSubstring(string str1, string str2)
         {
-            if (firstIndex == first.Length || secondIndex == second.Length)
+            return GetLongestCommonSubstring(str1, str2, 0, 0);
+        }
+
+        private int GetLongestCommonSubstring(string str1, string str2, int i, int j)
+        {
+            if (i == str1.Length || j == str2.Length)
             {
                 return 0;
             }
 
-            if (first[firstIndex] == second[secondIndex])
+            if (str1[i] == str2[j])
             {
-                return 1 + GetLongestCommonSubstring(first, second, firstIndex + 1, secondIndex + 1);
+                return 1 + GetLongestCommonSubstring(str1, str2, i + 1, j + 1);
             }
             else
             {
-                return Math.Max(
-                    GetLongestCommonSubstring(first, second, firstIndex, secondIndex + 1),
-                    GetLongestCommonSubstring(first, second, firstIndex + 1, secondIndex));
+                return Math.Max(GetLongestCommonSubstring(str1, str2, i, j + 1), GetLongestCommonSubstring(str1, str2, i + 1, j));
             }
         }
     }
--- src/LongestCommonSubstring/LongestCommonSubstringUnitTest.cs
+++ src/LongestCommonSubstring/LongestCommonSubstringUnitTest.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace LongestCommonSubstring
@@ -8,13 +7,71 @@ namespace LongestCommonSubstring
         [Fact]
         public void Test1()
         {
-            string first = "bd";
-            string second = "abcd";
+            string str1 = "bd";
+            string str2 = "abcd";
 
             var solution = new LongestCommonSubstringSolution();
-            int result = solution.GetLongestCommonSubstring(first, second);
+            int result1 = solution.GetLongestCommonSubstring(str1, str2);
+            int result2 = solution.GetLongestCommonSubstringBottomUp(str1, str2);
 
-            Assert.Equal(2, result);
+            Assert.Equal(2, result1);
+            Assert.Equal(2, result2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string str1 = "stone";
+            string str2 = "longest";
+
+            var solution = new LongestCommonSubstringSolution();
+            int result1 = solution.GetLongestCommonSubstring(str1, str2);
+            int result2 = solution.GetLongestCommonSubstringBottomUp(str1, str2);
+
+            Assert.Equal(3, result1);
+            Assert.Equal(3, result2);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string str1 = "abcdefghij";
+            string str2 = "cdgi";
+
+            var solution = new LongestCommonSubstringSolution();
+            int result1 = solution.GetLongestCommonSubstring(str1, str2);
+            int result2 = solution.GetLongestCommonSubstringBottomUp(str1, str2);
+
+            Assert.Equal(4, result1);
+            Assert.Equal(4, result2);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            string str1 = "abc";
+            string str2 = "abc";
+
+            var solution = new LongestCommonSubstringSolution();
+            int result1 = solution.GetLongestCommonSubstring(str1, str2);
+            int result2 = solution.GetLongestCommonSubstringBottomUp(str1, str2);
+
+            Assert.Equal(3, result1);
+            Assert.Equal(3, result2);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            string str1 = "abc";
+            string str2 = "deg";
+
+            var solution = new LongestCommonSubstringSolution();
+            int result1 = solution.GetLongestCommonSubstring(str1, str2);
+            int result2 = solution.GetLongestCommonSubstringBottomUp(str1, str2);
+
+            Assert.Equal(0, result1);
+            Assert.Equal(0, result2);
         }
     }
 }
